565e4bfae12c336ca9a536d800ebfaf5fe6dc518
Jesús Gabriel <jesus15202009@gmail.com>
2021-12-20 13:42:30 -0500

    Chat app basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/styles.scss';
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="chat">
+        <div className="chat__messages">
+          <div className="message">
+            <div className="message__text"><span className="message__text__text text">Hello world!</span></div>
+            <div className="message__info">
+              <span className="message__info__img" />
+              <p className="message__info__name text">Jesus Gabriel</p>
+              <small className="message__info__time">12:30pm</small>
+            </div>
+          </div>
+          <div className="message message--sent">
+            <div className="message--sent__text"><span className="message--sent__text__text text">Hello world!</span></div>
+            <div className="message--sent__info">
+              <span className="message--sent__info__img" />
+              <p className="message--sent__info__name text">You</p>
+              <small className="message--sent__info__time">12:30pm</small>
+            </div>
+          </div>
+          <div className="message message--sent">
+            <div className="message--sent__text"><span className="message--sent__text__text text">Hello world!</span></div>
+            <div className="message--sent__info">
+              <span className="message--sent__info__img" />
+              <p className="message--sent__info__name text">You</p>
+              <small className="message--sent__info__time">12:30pm</small>
+            </div>
+          </div>
+          <div className="message">
+            <div className="message__text"><span className="message__text__text text">Hello world!</span></div>
+            <div className="message__info">
+              <span className="message__info__img" />
+              <p className="message__info__name text">Jesus Gabriel</p>
+              <small className="message__info__time">12:30pm</small>
+            </div>
+          </div>
+          <div className="message">
+            <div className="message__text"><span className="message__text__text text">Hello world!</span></div>
+            <div className="message__info">
+              <span className="message__info__img" />
+              <p className="message__info__name text">Jesus Gabriel</p>
+              <small className="message__info__time">12:30pm</small>
+            </div>
+          </div>
+          <div className="message">
+            <div className="message__text"><span className="message__text__text text">Hello world!</span></div>
+            <div className="message__info">
+              <span className="message__info__img" />
+              <p className="message__info__name text">Jesus Gabriel</p>
+              <small className="message__info__time">12:30pm</small>
+            </div>
+          </div>
+        </div>
+        <div className="chat__actions">
+          <textarea className="textarea" placeholder="Write your message" />
+          <button className="button button--file" type="button">
+            <i className="fa fa-paperclip" />
+          </button>
+          <button className="button button--send" type="button">
+            Send
+            <i className="fa fa-paper-plane ml-i" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
