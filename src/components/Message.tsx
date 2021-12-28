106f61d3e30a6575a82f18aab7dbd17b677d48d8
Jesús Gabriel <jesus15202009@gmail.com>
2021-12-27 23:30:38 -0500

    Some css bugs, time and some more

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 export default function Message ({ message, sent }: any) {
+  console.log(message);
   return (
     <div className={`message ${sent && 'message--sent'}`}>
       <div className="message__text">
         <span className="message__text__text text">{message.message}</span>
       </div>
       <div className="message__info">
-        <img className="message__info__img" src={message.user.imageUrl} alt="profile" />
+        <img className="message__info__img" src={message.user.imageUrl} alt="" />
         <p className="message__info__name text">{message.user.name}</p>
-        <small className="message__info__time">12:30pm</small>
+        <small className="message__info__time">{message.time.toString()}</small>
       </div>
     </div>
   );
--- src/views/ChatView.tsx
+++ src/views/ChatView.tsx
@@ -1,19 +1,23 @@
 import React, { createRef, useEffect, useState } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 import Message from '../components/Message';
+import Loader from '../components/Loader';
 import { useAuthOut } from '../utils/auth/useAuth';
 import socket from '../utils/socket/socket';
 
 export default function ChatView ({ user }: any) {
   const messageText = createRef<any>();
   const [messages, setMessages] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  //  Auth handle for Logout
   const onSuccess = () => { socket.disconnect(); window.location.reload(); };
   const signOut = useAuthOut(onSuccess);
-
+  //  Socket event, it is listening when a message is received
   socket.on('hasANewMessage', (data: any) => setMessages([...messages, data]));
 
   const sendMessageHandler = () => {
-    const newMessage = { message: messageText.current.value, user };
+    const newMessage = { message: messageText.current.value, user, time: moment().format('LLL') };
     socket.emit('newMessage', newMessage);
     setMessages([...messages, newMessage]);
     messageText.current.value = '';
@@ -22,7 +26,8 @@ export default function ChatView ({ user }: any) {
   useEffect(() => {
     const getAllMessages = async () => {
       const { data } = await axios('http://localhost:5000/api/messages');
-      console.log(data);
+      setMessages([...messages, ...data]);
+      setLoading(false);
     };
     getAllMessages();
   }, []);
@@ -30,9 +35,14 @@ export default function ChatView ({ user }: any) {
   return (
     <div className="chat">
       <div className="chat__messages">
-        {
-          messages && messages.length > 0 && messages.map((message: any) => <Message message={message} sent={user.googleId === message.user.googleId && true} />)
-        }
+        {loading && <Loader />}
+        {messages.length > 0 && messages.map((message: any) => (
+          <Message
+            key={message.message}
+            message={message}
+            sent={user.googleId === message.user.googleId && true}
+          />
+        ))}
       </div>
       <div className="chat__actions">
         <textarea
